55399dfd9e9f74896a8a0abefa84150c41ef9000
cmxrenato <renatoaraujoleal@gmail.com>
2025-07-03 20:58:21 -0300

    finalização da lista

--- avancados/lista-encadeada.c
+++ avancados/lista-encadeada.c
@@ -15,10 +15,47 @@ struct No *novoNo(int valor){
 
 }
 
+void imprimirLista(struct No *inicio) {
+    struct No *atual = inicio;
+    while (atual != NULL) {
+        printf("Valor: %d\n", atual->valor);
+        atual = atual->prox;
+    }
+}
+void liberarLista(struct No *inicio) {
+    struct No *atual = inicio;
+    while (atual != NULL) {
+        struct No *prox = atual->prox;
+        free(atual);
+        atual = prox;
+    }
+}
 
 
 int main() {
 	struct No *lista = NULL;
+	char linha[20];
+	int valor;
+	int j = 1;
 
+
+while (j != 0) {
+    printf("Digite um novo valor para o Nó: ");
+    fgets(linha, sizeof(linha), stdin);
+    sscanf(linha, "%d", &valor);
+
+    struct No *novo = novoNo(valor);
+    novo->prox = lista;
+    lista = novo;
+
+    printf("\nAperte qualquer número para continuar ou 0 - parar: ");
+    fgets(linha, sizeof(linha), stdin);
+    sscanf(linha, "%d", &j);
+}
+
+
+  	imprimirLista(lista);   // Opcional, mas útil
+    	liberarLista(lista);    // Muito importante
+    	lista = NULL;  
     return 0;
 }
