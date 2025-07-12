7443ce3427c4681a79c74fe7585642c30e54e14c
cmxrenato <renatoaraujoleal@gmail.com>
2025-07-12 14:55:48 -0300

    lista-encadeada-ingles

--- avancados/lista-num-encadeada-ing.c
+++ avancados/lista-num-encadeada-ing.c
@@ -1,41 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct No {
-
+struct Node {
     int value;
-    struct No *nextNo;
-
+    struct Node *next;
 };
 
-struct No newNo(int value)
+struct Node *createNode(int value)
 {
-    
-
-
-
+    struct Node *newNode = (struct Node *) malloc(sizeof(struct Node));
+    if (newNode == NULL) {
+        printf("Error allocating memory.\n");
+        exit(1);
+    }
+    newNode->value = value;
+    newNode->next = NULL;
+    return newNode;
 }
 
+void printNodes(struct Node *head){
+    struct Node *current = head;
+    while(current != NULL){
+        printf("%d\n", current->value);
+        current = current->next;
+    }
+}
 
-
-
-
-
-
-
-
-
-
+void freeMemory(struct Node *head){
+    struct Node *current = head;
+    while(current != NULL){
+        struct Node *temp = current->next;
+        free(current);
+        current = temp;
+    }
+}
 
 int main()
 {
+    struct Node *head = NULL;
+    char input[20];
+    int value;
 
+    for(int i = 0; i < 5; i++){
+        printf("Enter an integer number: ");
+        fgets(input, sizeof(input), stdin);
+        sscanf(input, "%d", &value);
 
+        struct Node *newNode = createNode(value);
+        newNode->next = head;
+        head = newNode;
+    }
 
-
-
-
-
+    printNodes(head);
+    freeMemory(head);
+    head = NULL;
 
     return 0;
 }
