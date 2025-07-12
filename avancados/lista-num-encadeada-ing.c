dfbd02dfae7e6dc788f295a61c1f833a4ba1eff7
cmxrenato <renatoaraujoleal@gmail.com>
2025-07-12 00:47:19 -0300

    lista em ingles

---
+++ avancados/lista-num-encadeada-ing.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct No {
+
+    int value;
+    struct No *nextNo;
+
+};
+
+struct No newNo(int value)
+{
+    
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+int main()
+{
+
+
+
+
+
+
+
+
+    return 0;
+}
