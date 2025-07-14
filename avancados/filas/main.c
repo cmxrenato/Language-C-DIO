91f2cfa995ceb660ba176600336ce5066bb5fc2c
cmxrenato <renatoaraujoleal@gmail.com>
2025-07-13 23:22:11 -0300

    130725 23:22

--- avancados/filas/main.c
+++ avancados/filas/main.c
@@ -1,61 +1,80 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-struct Node{
-
+struct Node {
     int value;
     struct Node *nextNode;
-
+   
 };
 
 struct Node *newNode(int value)
 {
-    /* data */
     struct Node *newNode = (struct Node*) malloc(sizeof(struct Node));
-    if(newNode == NULL){
-        printf("Error allocating Memory.\n");
+    if (newNode == NULL) {
+        printf("Error allocating memory.\n");
         exit(1);
     }
 
     newNode->value = value;
     newNode->nextNode = NULL;
+   
     return newNode;
-
 }
 
-void addFila(int **value, struct Node **fila){
-    struct Node **newNode = createNode(*value);
-        (*newNode)->nextNode = *fila;
-        *fila = *newNode;
 
-          
+void addFila(int value, struct Node **fila)
+{
+    struct Node *novo = newNode(value);
+    struct Node *ultimo = NULL;
+    if(*fila == NULL){
+        *fila = novo;
+        return;
+    }
+    struct Node *current = *fila;
+    
+    while (current != NULL)
+    {
+        ultimo = current;
+        current = current->nextNode;
+    }
+    
+    
+    novo->nextNode = NULL;
+   
+    ultimo->nextNode = novo;
 
 }
 
 void printFila(struct Node *head)
 {
     struct Node *current = head;
-    while(current != NULL){
-        printf("%d\n",current->value);
+    while (current != NULL) {
+        printf("%d\n", current->value);
         current = current->nextNode;
-
     }
-
 }
+
 void freeFila(struct Node *head)
 {
     struct Node *current = head;
-    while(current != NULL){
+    while (current != NULL) {
         struct Node *temp = current->nextNode;
         free(current);
         current = temp;
     }
-
-
 }
 
 int main()
 {
     struct Node *fila = NULL;
+    addFila(1, &fila);
+    addFila(2, &fila);
+    addFila(3, &fila);
+    addFila(4, &fila);
+
+    printFila(fila);
+    freeFila(fila);
+    fila = NULL;
 
     return 0;
 }
